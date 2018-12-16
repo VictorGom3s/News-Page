f2d39f9141cb0105cb64a913cac0281e1bc23e13
Victor Gomes <vsampaio@pm.me>
2018-12-16 20:00:04 -0200

    Construindo layout da homepage

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,21 +1,47 @@
 <div class="container">
 	<div class="container">
 		<div class="row">
-			<div class="col-md-6">
+			<div class="col-md-6 coluna1">
 				<div class="row">
-					<div class="categoria-bg">Categoria 1.</div>
+					<div class="categoria-bg">						
+						Saúde					
+					</div>
 				</div>
 				<div class="row">
-					<div class="categoria-sm">Categoria 2.</div> 
+					<div class="row">
+						<div class="col-md-6">
+							<div class="categoria-sm">
+								Esportes
+							</div>						
+						</div>
+						<div class="col-md-6">
+							<div class="categoria-sm">
+								Tecnologia
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
-
-			<div class="col-md-6">
+			
+			<div class="col-md-6 coluna2">
 				<div class="row">
-					<div class="categoria-bg">Categoria 3.</div>
+					<div class="row">
+						<div class="col-md-6">
+							<div class="categoria-sm">
+								Cultura
+							</div>						
+						</div>
+						<div class="col-md-6">
+							<div class="categoria-sm">
+								Eventos
+							</div>
+						</div>
+					</div>
 				</div>
 				<div class="row">
-					<div class="categoria-bg">Categoria 4.</div>
+					<div class="categoria-bg">						
+							Música
+					</div>
 				</div>
 			</div>
 		</div>
